9c179eb6706c089f73ef2406fcd9e2b15b5e9520
Justine Gennaro <justine.rouzier@gmail.com>
2019-07-12 11:44:14 -0500

    working on tabs.js, added forEach, the tabs are showing up but not the content

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -14,7 +14,19 @@ const topics = document.querySelector('.topics')
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
 
 .then((response) => {
-    console.log('response: ', response)
+    console.log('response: ', response.data.topics)
+
+    const items = response.data.topics
+    console.log(items)
+
+    items.forEach(topic => {
+        const tab = document.createElement('div')
+        tab.classList.add('tab')
+        tab.textContent = items[topic]
+        topics.appendChild(tab)
+
+    });
+
 })
 
 .catch((error) => {
@@ -22,15 +34,42 @@ axios.get('https://lambda-times-backend.herokuapp.com/topics')
   })
 
 
-function createTabs() {
-
-    // create elements
-    const tab1 = document.createElement('div')
-    const tab2 = document.createElement('div')
-    const tab3 = document.createElement('div')
-    const tab4 = document.createElement('div')
-    const tab5 = document.createElement('div')
 
 
 
-  }
+
+// function createTabs(data) {
+
+//     // create elements
+//     const tab1 = document.createElement('div')
+//     const tab2 = document.createElement('div')
+//     const tab3 = document.createElement('div')
+//     const tab4 = document.createElement('div')
+//     const tab5 = document.createElement('div')
+
+//     // add classes
+//     tab1.classList.add('tab')
+//     tab2.classList.add('tab')
+//     tab3.classList.add('tab')
+//     tab4.classList.add('tab')
+//     tab5.classList.add('tab')
+
+//     // Add content
+//     tab1.textContent = data.topics[0]
+//     tab2.textContent = data.topics[1]
+//     tab3.textContent = data.topics[2]
+//     tab4.textContent = data.topics[3]
+//     tab5.textContent = data.topics[4]
+
+//     // Append
+//     topics.appendChild(tab1)
+//     topics.appendChild(tab2)
+//     topics.appendChild(tab3)
+//     topics.appendChild(tab4)
+//     topics.appendChild(tab5)
+
+ 
+
+//   }
+
+//   createTabs()
